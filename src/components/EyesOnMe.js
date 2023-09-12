52d5971194ecdf8250496b6af3a1a2f20bd5c510
Navita Ramasar <navitaramasar14@gmail.com>
2023-09-12 12:21:22 -0400

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from "react"
+
+export default function EyesOnMe(){
+    function Focus(){
+        console.log("Good!")
+    }
+
+    function Blur(){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <button onFocus={Focus} onBlur={Blur}>Eyes On Me</button>
+    )
+}
